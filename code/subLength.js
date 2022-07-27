41e8ca73cc6d09229fb5b55ea94bb0122fce49ff
Nury Amanmadov <amanmadov@gmail.com>
2022-07-27 13:50:46 -0700

    added credit card checker file

--- code/subLength.js
+++ code/subLength.js
@@ -24,7 +24,7 @@ const subLength = (str, letter) => {
     return secondOcc - firstOcc + 1;
 }
 
-console.log(subLength('Saaturday', 'a'));   // returns 6
+console.log(subLength('Saturday', 'a'));    // returns 6
 console.log(subLength('summer', 'm'));      // returns 2
 console.log(subLength('digitize', 'i'));    // returns 0
 console.log(subLength('cheesecake', 'k'));  // returns 0
@@ -33,16 +33,16 @@ console.log(subLength('cheesecake', 'k'));  // returns 0
 
 // refactored code:
 const subLength = function (str, chr) {
-    if (str.toLowerCase().split('').filter(x => x === chr).length !== 2) return 0;
+    if (str.toLowerCase().split('').filter(letter => letter === chr).length !== 2) return 0;
     let firstIndex = lastIndex = 0;
-    str.split('').forEach((el, ind) => {
-        if (el === chr && firstIndex === 0) firstIndex = ind;
-        if (el === chr && firstIndex !== 0) lastIndex = ind;
+    str.split('').forEach((letter, ind) => {
+        if (letter === chr && firstIndex === 0) firstIndex = ind;
+        if (letter === chr && firstIndex !== 0) lastIndex = ind;
     })
     return lastIndex - firstIndex + 1;
 }
 
-console.log(subLength('Saaturday', 'a'));   // returns 6
+console.log(subLength('Saturday', 'a'));    // returns 6
 console.log(subLength('summer', 'm'));      // returns 2
 console.log(subLength('digitize', 'i'));    // returns 0
 console.log(subLength('cheesecake', 'k'));  // returns 0
