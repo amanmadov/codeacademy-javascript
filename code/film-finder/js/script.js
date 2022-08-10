a34d42bbeb41cdb92a9a3f2a44839a7ac2604eff
Nury Amanmadov <amanmadov@gmail.com>
2022-08-09 20:04:46 -0700

    Update script.js

--- code/film-finder/js/script.js
+++ code/film-finder/js/script.js
@@ -1,4 +1,4 @@
-const tmdbKey = "3e2c6cf8cf3368c852c292789e054b7b";
+const tmdbKey = "myKey";
 const tmdbBaseUrl = "https://api.themoviedb.org/3/";
 const playBtn = document.getElementById("playBtn");
 
